21345050efc86372df2e4393b093d31486dc6cd4
Luis Fernando <lcampomontero@hotmail.com>
2019-07-29 18:41:27 -0500

    nav_bar

--- HelloSchool/ClientApp/src/app/models/preregistro.ts
+++ HelloSchool/ClientApp/src/app/models/preregistro.ts
@@ -1,20 +1,20 @@
 export class Preregistro {
         Id: number;
-        TipoDocumento:string;
-        NoDocumento:string;
-        Apellidos:string;
-        Nombres:string; 
-         Correo:string;
-         Celular:string; 
-         Parentesco:string;
-         TipoDocumentoEstudiante:string;
-         NoDocumentoEstudiante:string;
-         ApellidosEstudiante:string;
-         NombresEstudiante:string;
-         FechaNacimientoEstudiante :Date;
-          Estado:number;
+        TipoDocumento: string;
+        NoDocumento: string;
+        Apellidos: string;
+        Nombres: string;
+         Correo: string;
+         Celular: string;
+         Parentesco: string;
+         TipoDocumentoEstudiante: string;
+         NoDocumentoEstudiante: string;
+         ApellidosEstudiante: string;
+         NombresEstudiante: string;
+         FechaNacimientoEstudiante: Date;
+          Estado: number;
 
     }
-    
+
 
 
